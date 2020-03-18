7ab5015d9ec12faea08644daded39da066f36242
Chris Dzoba <Chris@Dzoba.com>
2020-03-18 15:40:23 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,6 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 # export PUPPETEER_EXEC_PATH="google-chrome-stable"
 
-# sudo chown -R pptruser:pptruser /github/workspace
-mkdir -p /github/home
-sh -c "sudo chown -R pptruser:pptruser /github"
-sh -c "sudo chmod -R pptruser:pptruser /github"
-
-
 echo "Inside The Docker Container"
 
 # Run commands
